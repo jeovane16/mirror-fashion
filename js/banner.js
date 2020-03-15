2bee528b424ad83aca5d6f656205b4fbe0ba10d5
jeovane16 <jeovanearaujo16@gmail.com>
2020-03-15 19:56:35 -0300

    Interatividade com js

--- js/banner.js
+++ js/banner.js
@@ -6,4 +6,18 @@ function trocaBanner() {
     document.querySelector('.banner-destaque img').src = banners[bannerAtual];
 }
 
-setInterval(trocaBanner, 4000);
+var timer = setInterval(trocaBanner, 4000);
+
+var controle = document.querySelector('.pause');
+
+controle.onclick = function() {
+    if (controle.className == 'pause') {
+        clearInterval(timer);
+        controle.className = 'play';
+    } else {
+        timer = setInterval(trocaBanner, 4000);
+        controle.className = 'pause';
+    }
+
+    return false;
+}
--- js/produto.js
+++ js/produto.js
@@ -1,8 +1,6 @@
 var inputTamanho = document.querySelector('[name=tamanho]');
 var outputTamanho = document.querySelector('[name=valortamanho]');
 
-function mostraTamanho(){
-    outputTamanho.value = inputTamanho.value
+inputTamanho.oninput = function () {
+    outputTamanho.value = inputTamanho.value;
 }
-
-inputTamanho.oninput = mostraTamanho;
